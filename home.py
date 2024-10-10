bed25341b69a98ccc279826203ae46e604c457fc
DanielRodriguez-SS <83928934+DanielRodriguez-SS@users.noreply.github.com>
2024-10-10 11:20:00 +1030

    Update home.py

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ def hide_streamlit_defualt_menu_footer():
 def set_footer():
    footer = f'''
             <footer style="background-color: rgb(16, 18, 22);color: rgb(69, 70, 76);text-align: right;padding: 10px;position: fixed;bottom: 0;border-top-left-radius: 15px;border-top-right-radius: 15px;z-index: 1000;">
-               <p>&copy; {datetime.now().strftime('%Y')} Daniel Rodriguez. All rights reserved.</p>
+               <p>&copy; Daniel Rodriguez. All rights reserved.</p>
             </footer>
             '''
    st.markdown(footer, unsafe_allow_html=True)
