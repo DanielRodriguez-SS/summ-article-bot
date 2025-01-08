6810cd202d0ac1e7301c2586c1a00457cc763add
SWimplifyDev <Daniel.rodriguez.marin86@gmail.com>
2025-01-08 11:00:25 +1030

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,24 @@
 import streamlit as st
 from openai import OpenAI
+<<<<<<< HEAD
+####
+=======
 
+st.set_page_config(page_title="ArticleBot",
+                       page_icon="🎛️")
+
+def hide_streamlit_defualt_menu_footer():
+   hide_menu_style = """
+            <style>
+            .stAppToolbar {visibility: hidden;}
+            ._link_19b1q_10 {visibility: hidden;}
+            </style>
+            """
+   st.markdown(hide_menu_style, unsafe_allow_html=True)
+    
+hide_streamlit_defualt_menu_footer()
+
+>>>>>>> af78c17e406c40355e3573fcde674832a002598d
 st.title("Article Summarization Bot")
 st.warning("We appreciate your engagement! Please note, this demo is designed to process a maximum of 3 interactions and may be unavailable if too many people use the service concurrently. Thank you for your understanding.")
 
